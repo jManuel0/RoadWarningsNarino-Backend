bc7cdb5f9edd7a7445f0f5d3b417b4801bb2c5ce
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-06 10:38:35 -0500

    primer commit

---
+++ RoadWarningsnarino-backend/src/main/java/com/roadwarnings/RoadWarningsnarino/RoadWarningsnarinoApplication.java
@@ -0,0 +1,13 @@
+package com.roadwarnings.RoadWarningsnarino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadWarningsnarinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadWarningsnarinoApplication.class, args);
+	}
+
+}
