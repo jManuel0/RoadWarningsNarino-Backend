650dd9eeb34628fdadb3f3458de1ad8a43cbff68
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-08 11:33:58 -0500

    Modificacion de archivos

---
+++ src/main/java/com/roadwarnings/narino/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.roadwarnings.narino.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+public class TestController {
+    
+@GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+
+
+
+}
