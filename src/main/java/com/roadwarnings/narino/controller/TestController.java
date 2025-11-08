42547e2360a96ff8e1cb75ceda80256798e07675
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-08 11:44:47 -0500

    Modificacion de archivos

--- src/main/java/com/roadwarnings/narino/controller/TestController.java
+++ src/main/java/com/roadwarnings/narino/controller/TestController.java
@@ -2,18 +2,16 @@ package com.roadwarnings.narino.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
-@CrossOrigin(origins = "http://localhost:8080")
+@RequestMapping("/api") // 👈 tus rutas comienzan con /api según tu mensaje en consola
+@CrossOrigin(origins = "http://localhost:5173")
 public class TestController {
-    
-@GetMapping("/ping")
+
+    @GetMapping("/ping")
     public String ping() {
         return "pong";
     }
-
-
-
 }
