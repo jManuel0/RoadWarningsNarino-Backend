c2abe9e6b3e278d463fe8c4670cd85eb3a37ead0
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-08 14:42:10 -0500

    modificacion de archivos

--- src/main/java/com/roadwarnings/narino/dto/response/GasStationResponseDTO.java
+++ src/main/java/com/roadwarnings/narino/dto/response/GasStationResponseDTO.java
@@ -1,5 +1,32 @@
 package com.roadwarnings.narino.dto.response;
 
+import lombok.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GasStationResponseDTO {
-    
+
+    private Long id;
+    private String name;
+    private String brand;
+    private Double latitude;
+    private Double longitude;
+    private String address;
+    private String municipality;
+    private String phoneNumber;
+    private Boolean hasGasoline;
+    private Boolean hasDiesel;
+    private BigDecimal gasolinePrice;
+    private BigDecimal dieselPrice;
+    private Boolean isOpen24Hours;
+    private String openingTime;
+    private String closingTime;
+    private Boolean isAvailable;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
