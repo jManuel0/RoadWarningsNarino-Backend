6d8ec6209515c8b6939116050d6eacbe08a9d552
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-08 11:42:46 -0500

    Modificacion de archivos

--- src/main/java/com/roadwarnings/narino/controller/TestController.java
+++ src/main/java/com/roadwarnings/narino/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:8080")
 public class TestController {
     
 @GetMapping("/ping")
