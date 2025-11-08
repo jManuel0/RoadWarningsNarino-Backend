66960c3eb568f44a8ff2a0ff0583a86294b417db
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-08 14:58:55 -0500

    modificacion de archivos

--- src/main/java/com/roadwarnings/narino/controller/GasStationController.java
+++ src/main/java/com/roadwarnings/narino/controller/GasStationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/GasStations")
+@RequestMapping("/api/gas-stations")
 @RequiredArgsConstructor
 public class GasStationController {
 
