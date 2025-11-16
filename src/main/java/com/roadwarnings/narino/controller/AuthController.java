9f8f7d15ad6d626657ec8cbab35ea9b47590e075
jManuel0 <juan.ordonezar@campusucc.edu.co>
2025-11-16 00:53:12 -0500

    Actualizacion de AuthController

--- src/main/java/com/roadwarnings/narino/controller/AuthController.java
+++ src/main/java/com/roadwarnings/narino/controller/AuthController.java
@@ -17,7 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class AuthController {
